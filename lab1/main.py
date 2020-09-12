1beb5ec5abc681bdb29dbdc0a989dcff2f774021
kiwidancebad <morelessequal@gmail.com>
2020-09-12 15:20:09 +0300

    refactor: printing

--- lab1/main.py
+++ lab1/main.py
@@ -15,18 +15,19 @@ X_b = np.c_[np.ones((len(X), 1)), X]
 
 theta, cost_history, theta_history = gradient_descent(X_b, y, theta, step, iterations)
 
-print('Gradient descent')
+print('\n\n')
+print('Gradient descent\n-------------')
 print('Theta0: {:0.3f} \nTheta1: {:0.3f}'.format(theta[0][0], theta[1][0]))
-print('Final cost: {:0.3f}'.format(cost_history[-1]))
+print('Final cost: {:0.3f}\n\n'.format(cost_history[-1]))
 
 # theta, cost_history = minibatch_gradient_descent(X_b, y, theta, step, iterations)
 
-# print('Minibatch gradient descent')
+# print('Minibatch gradient descent\n-------------')
 # print('Theta0: {:0.3f} \nTheta1: {:0.3f}'.format(theta[0][0], theta[1][0]))
-# print('Final cost: {:0.3f}'.format(cost_history[-1]))
+# print('Final cost: {:0.3f}\n\n'.format(cost_history[-1]))
 
 theta, cost_history = stocashtic_gradient_descent(X_b, y, theta, step, iterations)
 
-print('Stocashtic gradient descent')
+print('Stocashtic gradient descent\n-------------')
 print('Theta0: {:0.3f} \nTheta1: {:0.3f}'.format(theta[0][0], theta[1][0]))
-print('Final cost: {:0.3f}'.format(cost_history[-1]))
+print('Final cost: {:0.3f}\n\n'.format(cost_history[-1]))
